10b27c43071108001d0ef370dd7eaa507eb30084
Jack Zhang <jack333zhang@gmail.com>
2022-12-18 17:41:10 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
