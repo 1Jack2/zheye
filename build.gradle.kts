09cf1af40061d3a559cd6270b5eb09799478cc1f
Jack Zhang <jack333zhang@gmail.com>
2022-12-18 18:14:46 +0800

    Add spring boot and implement the endpoint for the health check

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
 plugins {
     java
+    id("org.springframework.boot") version "2.7.6"
 }
 
 repositories {
@@ -7,9 +10,15 @@ repositories {
 }
 
 dependencies {
+    implementation(platform(SpringBootPlugin.BOM_COORDINATES))
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 java {
