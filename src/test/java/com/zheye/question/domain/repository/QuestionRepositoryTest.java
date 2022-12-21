5e1326ed6d92e9a585405985f7add93788ee71f9
Jack Zhang <jack333zhang@gmail.com>
2022-12-21 21:34:10 +0800

    Alter queston id type from serial to text

--- src/test/java/com/zheye/question/domain/repository/QuestionRepositoryTest.java
+++ src/test/java/com/zheye/question/domain/repository/QuestionRepositoryTest.java
@@ -2,8 +2,11 @@ package com.zheye.question.domain.repository;
 
 import com.zheye.question.core.JpaRepositoryTest;
 import com.zheye.question.domain.model.Question;
+import junit.framework.AssertionFailedError;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -16,13 +19,29 @@ class QuestionRepositoryTest {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @Test
     void repository_should_successfully_save_question() {
         Question question = new Question("UID_00001", "A test title", "A test detail");
         Question savedQuestion = questionRepository.save(question);
         assertThat(savedQuestion.getId(), is(notNullValue()));
-        assertThat(savedQuestion.getQuestionerId(), equalTo(question.getQuestionerId()));
-        assertThat(savedQuestion.getTitle(), equalTo(question.getTitle()));
-        assertThat(savedQuestion.getDetail(), equalTo(question.getDetail()));
+        assertSameQuestion(savedQuestion, question);
+    }
+
+    @Test
+    void repository_should_successfully_find_by_id() {
+        Question question = new Question("UID_00001", "A test title", "A test detail");
+        Question savedQuestion = questionRepository.saveAndFlush(question);
+        entityManager.detach(savedQuestion);
+        Question foundQuestion = questionRepository.findById(savedQuestion.getId()).orElseThrow(AssertionFailedError::new);
+        assertSameQuestion(foundQuestion, question);
+    }
+
+    private static void assertSameQuestion(Question actualQuestion, Question expectQuestion) {
+        assertThat(actualQuestion.getQuestionerId(), equalTo(expectQuestion.getQuestionerId()));
+        assertThat(actualQuestion.getTitle(), equalTo(expectQuestion.getTitle()));
+        assertThat(actualQuestion.getDetail(), equalTo(expectQuestion.getDetail()));
     }
 }
